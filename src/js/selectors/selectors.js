67608acb1ba0837255cf187e47a04860b38470f3
Ux1ew1 <ux1ew10220@gmail.com>
2024-05-20 15:41:23 +0300

    Селекторы для функций

--- src/js/selectors/selectors.js
+++ src/js/selectors/selectors.js
@@ -1,2 +1,3 @@
-export const productList = document.querySelector('.products-list') // контейнер для отрисовки на основной странице
-export const form = document.querySelector('form') // Форма обработки данных 
+export const productList = document.querySelector('.products-list') // Контейнер для отрисовки товаров на основной странице.
+export const form = document.querySelector('form') // Форма обработки данных.
+export const paginationList = document.querySelector('.pagination') // Контейнер для пагинации
